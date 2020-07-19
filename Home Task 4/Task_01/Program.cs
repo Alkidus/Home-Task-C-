1b7dc04e544578995c883b40a70064574b977b59
Alkidus <68506257+Alkidus@users.noreply.github.com>
2020-07-19 18:30:53 +0300

    Add files via upload
    Разработать абстрактный класс «Геометрическая Фигура» с методами «Площадь Фигуры» и «Периметр Фигуры».
    Разработать классы-наследники: Треугольник, Квадрат,
    Ромб, Прямоугольник, Параллелограмм, Трапеция, Круг,
    Эллипс.Реализовать конструкторы, которые однозначно
    определяют объекты данных классов.
    Реализовать класс «Составная Фигура», который
    может состоять из любого количества «Геометрических
    Фигур». Для данного класса определить метод нахождения
    площади фигуры.Создать диаграмму взаимоотношений классов.

---
+++ Home Task 4/Task_01/Program.cs
@@ -0,0 +1,283 @@
+﻿/*Задание 1.
+Разработать абстрактный класс «Геометрическая Фигура» с методами «Площадь Фигуры» и «Периметр Фигуры».
+Разработать классы-наследники: Треугольник, Квадрат,
+Ромб, Прямоугольник, Параллелограмм, Трапеция, Круг,
+Эллипс.Реализовать конструкторы, которые однозначно
+определяют объекты данных классов.
+Реализовать класс «Составная Фигура», который
+может состоять из любого количества «Геометрических
+Фигур». Для данного класса определить метод нахождения
+площади фигуры.Создать диаграмму взаимоотношений классов.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Task_01
+{
+    public abstract class GeometricFigure
+    {
+        public string _typeFigure;
+        public GeometricFigure(string typeFigure)
+        {
+            _typeFigure = typeFigure;
+        }
+        public abstract double Area();
+        public abstract double Perim();
+        public override string ToString()
+        {
+            return $"\n\t\tТип фигуры: {_typeFigure} \nПерметр равен: {Perim()} \nПлощадь равна: {Area()}";
+        }
+    }
+    class Circle : GeometricFigure
+    {
+        double _radius;
+        public Circle (string typeFigure, double radius) : base(typeFigure)
+        {
+            _radius = radius;
+        }
+        public double getRadius(double rad) { return _radius = rad; }
+        public void setRadius()
+        {
+            Write("\nВведите радиус окружности: ");
+            double rad1 = Convert.ToDouble(ReadLine());
+            getRadius(rad1);
+        }
+        public override double Perim()
+        {
+            return 2 * 3.1415 * _radius;
+        }
+        public override double Area()
+        {
+            return 3.1415 * _radius * _radius;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nРадиус {_typeFigure} равен {_radius}";
+        }
+    }
+    class Ellips : GeometricFigure
+    {
+        double a, b;
+        public Ellips (string typeFigure, double _a, double _b) : base(typeFigure)
+        {
+            a = _a;
+            b = _b;
+        }
+
+        public override double Perim()
+        {
+            return 4*(3.1415*a*b + (a-b))/(a+b);
+        }
+
+        public override double Area()
+        {
+            return 3.1415*a*b;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nмалая полуось равена {(a<b?a:b)} \nбольшая полуось равна {(b>a?b:a)}";
+        }
+    }
+    class Triangle : GeometricFigure
+    {
+        double a, b, c;
+        public Triangle(string typeFigure, double _a, double _b, double _c) : base(typeFigure)
+        {
+            a = _a;
+            b = _b;
+            c = _c;
+        }
+        public override double Perim()
+        {
+            return a + b + c;
+        }
+        public override double Area()
+        {
+            double p = 0.5 * (a + b + c);
+            return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nСтороны треугольника равны А = {a}, B = {b}, C = {c}";
+        }
+    }
+    class Square : GeometricFigure
+    {
+        double a;
+        public Square(string typeFigure, double _a) : base(typeFigure)
+        {
+            a = _a;
+        }
+        public override double Perim()
+        {
+            return 4 * a;
+        }
+        public override double Area()
+        {
+            return a * a;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nСторона квадрата равна А = {a}";
+        }
+    }
+    class Rhombus : GeometricFigure
+    {
+        double a, h;
+        public Rhombus(string typeFigure, double _a, double _h) : base(typeFigure)
+        {
+            a = _a;
+            h = _h;
+        }
+        public override double Perim()
+        {
+            return 4 * a;
+        }
+        public override double Area()
+        {
+            return a * h;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nДлина основания ромба А = {a}, длина высоты ромба Н = {h}";
+        }
+    }
+    class Rectangle : GeometricFigure
+    {
+        double a, b;
+        public Rectangle(string typeFigure, double _a, double _b) : base(typeFigure)
+        {
+            a = _a;
+            b = _b;
+        }
+        public override double Perim()
+        {
+            return 2 * (a + b);
+        }
+        public override double Area()
+        {
+            return a * b;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nДлина основания прямоугольника А = {a}, длина высоты прямоугольника В = {b}";
+        }
+    }
+    class Parallelogram : GeometricFigure
+    {
+        double a, b, h;
+        public Parallelogram(string typeFigure, double _a, double _b, double _h) : base(typeFigure)
+        {
+            a = _a;
+            b = _b;
+            h = _h;
+        }
+        public override double Perim()
+        {
+            return 2 * (a + b);
+        }
+        public override double Area()
+        {
+            return a * h;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nДлина основания параллелограмма А = {a}, длина высоты параллелограмма h = {h}";
+        }
+    }
+    class Trapezoid : GeometricFigure
+    {
+        double a, b, c1, c2, h;
+        public Trapezoid(string typeFigure, double _a, double _b, double _c1, double _c2, double _h) : base(typeFigure)
+        {
+            a = _a;
+            b = _b;
+            c1 = _c1;
+            c2 = _c2;
+            h = _h;
+        }
+        public override double Perim()
+        {
+            return a + b + c1 + c2;
+        }
+        public override double Area()
+        {
+            return 0.5 * (a + b) * h;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nОснование трапеции А = {a}, длина высоты трапеции h = {h}";
+        }
+    }
+    class SostavFigure : GeometricFigure
+    {
+        public SostavFigure(string typeFigure) : base(typeFigure)
+        {
+        }
+        public double allArea = 0;
+        public override double Perim()
+        {
+            throw new NotImplementedException();
+        }
+        public override double Area()
+        {
+            throw new NotImplementedException();
+        }
+        public double sostavArea(GeometricFigure[] fig)
+        {
+            foreach (GeometricFigure item in fig)
+            {
+                allArea += item.Area();
+            }
+            return allArea;
+        }
+        public void Show()
+        {
+            WriteLine($"\nОбщая площадь {_typeFigure} равна {allArea}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //можно раскомментировать добавленные фигуры или добавить новые в масссив
+            //тем самым будет меняться Площадь Составной фигуры
+            GeometricFigure[] figures =
+            {
+                //new Circle("ОКРУЖНОСТЬ", 17),
+                //new Ellips("ЭЛЛИПС", 24, 17.5),
+                //new Triangle("ТРЕУГОЛЬНИК", 3, 4, 5),
+                //new Square ("КВАДРАТ", 11),
+                //new Rhombus ("РОМБ", 17, 4),
+                new Rectangle ("ПРЯМОУГОЛЬНИК", 7, 12),
+                new Parallelogram ("ПАРАЛЛЕЛОГРАММ", 14.5, 12.5, 8),
+                new Trapezoid ("ТРАПЕЦИЯ", 18, 10, 11, 12, 8),
+            };
+
+            foreach (GeometricFigure item in figures)
+            {
+                WriteLine(item);
+                item.Perim();
+                item.Area();
+            }
+
+            SostavFigure obj = new SostavFigure("Составная Фигура");
+            obj.sostavArea(figures);
+            obj.Show();
+
+            //Circle circle1 = new Circle("OKRUG", 0);
+            //circle1.setRadius();
+            //WriteLine(circle1);
+            //circle1.Perim();
+            //circle1.Area();
+            
+            ReadKey();
+        }
+    }
+}
---
+++ Home Task 4/Task_02/Program.cs
@@ -0,0 +1,165 @@
+﻿/*Задание 2.
+Разработать архитектуру классов иерархии товаров
+при разработке системы управления потоками товаров для
+дистрибьюторской компании.Прописать члены классов.
+Создать диаграммы взаимоотношений классов.
+Должны быть предусмотрены разные типы товаров,
+в том числе:
+• бытовая химия;
+• продукты питания.
+Предусмотреть классы управления потоком товаров
+(пришло, реализовано, списано, передано).*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task_02
+{
+    public abstract class Tovar
+    {
+        public string name;
+        public int number;
+        public double price;
+        public Tovar (string name, int number, double price)
+        {
+            this.name = name;
+            this.number = number;
+            this.price = price;
+        }
+        public abstract override string ToString();
+    }
+    public class Chemistry : Tovar
+    {
+        public string category;
+        public Chemistry(string name, int number, double price, string category) : base(name, number, price)
+        {
+            this.category = category;
+        }
+        public override string ToString()
+        {
+            return string.Format($"\nНа складе сейчас в\nКатегория товаров: {category}\nНаименование: {name}\nКоличество: {number}\nЦена: {price}\n");
+        }
+    }
+    public class Cleaning : Chemistry
+    {
+        public string otdel;
+        public Cleaning(string name, int number, double price, string category, string otdel) : base(name, number, price, category)
+        {
+            this.otdel = otdel;
+        }
+    }
+    public class Bathroom : Cleaning
+    {
+        public Bathroom(string name, int number, double price, string category, string otdel) : base(name, number, price, category, otdel)
+        {
+        }
+        public override string ToString()
+        {
+            return string.Format($"\nНа складе сейчас в\nКатегория товаров: {category}\nНаименование: {name}\nКоличество: {number}\nЦена: {price}\n");
+        }
+    }
+    public class Dishes : Cleaning
+    {
+        public Dishes(string name, int number, double price, string category, string otdel) : base(name, number, price, category, otdel)
+        {
+        }
+        public override string ToString()
+        {
+            return string.Format($"\nНа складе сейчас в\nКатегория товаров: {category}\nНаименование: {name}\nКоличество: {number}\nЦена: {price}\n");
+        }
+    }
+    public class Foods : Tovar
+    {
+        public string category;
+        public Foods(string name, int number, double price, string category) : base(name, number, price)
+        {
+            this.category = category;
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Alkogol : Foods
+    {
+        public string otdel;
+        public Alkogol(string name, int number, double price, string category, string otdel) : base(name, number, price, category)
+        {
+            this.otdel = otdel;
+        }
+    }
+    public class Whiskey : Alkogol
+    {
+        public Whiskey(string name, int number, double price, string category, string otdel) : base(name, number, price, category, otdel)
+        {
+        }
+        public override string ToString()
+        {
+            return string.Format($"\nНа складе сейчас в\nКатегория товаров: {category}\nНаименование: {name}\nКоличество: {number}\nЦена: {price}\n");
+        }
+    }
+    public class Beer : Alkogol
+    {
+        public Beer(string name, int number, double price, string category, string otdel) : base(name, number, price, category, otdel)
+        {
+        }
+        public override string ToString()
+        {
+            return string.Format($"\nНа складе сейчас в\nКатегория товаров: {category}\nНаименование: {name}\nКоличество: {number}\nЦена: {price}\n");
+        }
+    }
+    enum Position
+    {
+        Postupil, Realizovan, Spisan, Peredan
+    }
+    class Potok
+    {
+        public Tovar[] Lot;
+        public Position Condition;
+
+        public Potok(Tovar[] lot, Position condition)
+        {
+            Lot = lot;
+            Condition = condition;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"\nУстановить новый статус:  {Condition}");
+        }
+
+        public void Print()
+        {
+            foreach (var item in Lot)
+            {
+                WriteLine($"Статус:  {Condition}");
+                WriteLine(item);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tovar[] spisok =
+            {
+                new Beer("Чешское", 20, 19.99, "Подукты питания", "Алкоголь"),
+                new Whiskey("Wild Turkey", 12, 450.99, "Подукты питания", "Алкоголь"),
+                new Bathroom("Туалетный утенок", 112, 56.99, "Бытовая химия", "Чистящие средства"),
+                new Dishes("Гала", 35, 44.75, "Бытовая химия", "Моющие для посуды")
+            };
+            Potok tovar1 = new Potok(spisok, Position.Postupil);
+            tovar1.Print();
+            tovar1.Condition = Position.Realizovan;
+            WriteLine(tovar1.ToString());
+            tovar1.Print();
+
+            ReadKey();
+        }
+    }
+}
